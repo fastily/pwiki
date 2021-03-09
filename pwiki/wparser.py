a0041b81f59f78e68ee54ac382bf9ecab733e3b0
fastily <fastily@users.noreply.github.com>
2021-03-09 03:41:15 -0800

    more docstrings, better edge case handling in wparser

--- pwiki/wparser.py
+++ pwiki/wparser.py
@@ -9,6 +9,9 @@ from contextlib import suppress
 from typing import Any, KeysView, TYPE_CHECKING, Union, ValuesView
 from xml.etree import ElementTree
 
+from .ns import NS
+from .oquery import OQuery
+
 from .utils import make_params, mine_for
 
 if TYPE_CHECKING:
@@ -26,7 +29,7 @@ class WikiText:
         Args:
             elements (Union[str, WikiTemplate], optional): Default values to initialize this WikiText with.  These will be appended together in the order passed.
         """
-        self.l: list = []
+        self._l: list = []
 
         for e in elements:
             self += e
@@ -37,7 +40,7 @@ class WikiText:
         Returns:
             bool: `True` if this WikiText is not empty.
         """
-        return bool(self.l)
+        return bool(self._l)
 
     def __iadd__(self, other: Any) -> WikiText:
         """Appends the specified element to the end of this WikiText object.  CAVEAT: if `other` is a `WikiText` object, then other's contents will be merged into this `WikiText`. 
@@ -54,15 +57,15 @@ class WikiText:
         if isinstance(other, str):
             if not other:
                 pass
-            elif self.l and isinstance(self.l[-1], str):
-                self.l[-1] += other
+            elif self._l and isinstance(self._l[-1], str):
+                self._l[-1] += other
             else:
-                self.l.append(other)
+                self._l.append(other)
         elif isinstance(other, WikiTemplate):
-            self.l.append(other)
+            self._l.append(other)
             other.parent = self
         elif isinstance(other, WikiText):
-            for e in other.l:
+            for e in other._l:
                 self += e
         else:
             raise TypeError(f"'{other}' is not a valid type (str, WikiTemplate) for appending to this WikiText")
@@ -84,7 +87,7 @@ class WikiText:
         Returns:
             list[WikiTemplate]: A list of `WikiTemplate` objects contained in this `WikiText` (top level only)
         """
-        return [x for x in self.l if isinstance(x, WikiTemplate)]
+        return [x for x in self._l if isinstance(x, WikiTemplate)]
 
     def all_templates(self) -> list[WikiTemplate]:
         """Recursively finds all templates contained in this `WikiText` and their subtemplates.
@@ -109,7 +112,7 @@ class WikiText:
         Returns:
             str: A `str` representation of this `WikiText`.
         """
-        out = "".join(str(x) for x in self.l)
+        out = "".join(str(x) for x in self._l)
         return out.strip() if trim else out
 
 
@@ -125,7 +128,7 @@ class WikiTemplate:
         """
         self.parent: WikiText = parent
         self.title: str = title
-        self.params: dict[str, WikiText] = {}
+        self._params: dict[str, WikiText] = {}
 
     def __contains__(self, item: Any) -> bool:
         """Check if the key `item` is the name of a parameter
@@ -136,7 +139,7 @@ class WikiTemplate:
         Returns:
             bool: `True` if the key `item` is the name of a parameter with a non-empty value in this WikiTemplate.
         """
-        return item in self.params
+        return item in self._params
 
     def __getitem__(self, key: Any) -> WikiText:
         """Returns the parameter value associated with `key` in this `WikiTemplate`'s params
@@ -153,7 +156,7 @@ class WikiTemplate:
         if key not in self:
             raise KeyError(f"'{key}' is not in this WikiTemplate object!")
 
-        return self.params[key]
+        return self._params[key]
 
     def __setitem__(self, key: Any, value: Any):
         """Associates `key` and `value` as entries in this `WikiTemplate`'s parameter list.
@@ -170,7 +173,7 @@ class WikiTemplate:
         elif not isinstance(value, (str, WikiTemplate, WikiText)):
             raise TypeError(f"{value} is not an acceptable parameter type for WikiTemplate")
 
-        self.params[key] = value if isinstance(value, WikiText) else WikiText(value)
+        self._params[key] = value if isinstance(value, WikiText) else WikiText(value)
 
     def __str__(self) -> str:
         """Generates a `str` representaiton of this WikiTemplate.
@@ -190,7 +193,7 @@ class WikiTemplate:
         Returns:
             bool: `True` if `key` exists in this `WikiTemplate`.  If `empty_ok` is `False`, then `True` will be returned if the value assoaciated with `key` is also non-empty.
         """
-        return key in self and (empty_ok or bool(self.params.get(key)))
+        return key in self and (empty_ok or bool(self._params.get(key)))
 
     def pop(self, k: str) -> WikiText:
         """Removes the key, `k`, and its associated value from this `WikiTemplate`'s parameters, and then return the value.
@@ -202,12 +205,12 @@ class WikiTemplate:
             WikiText: The value formerly associated with `k`.  `None` if `k` is not in this `WikiTemplate`.
         """
         with suppress(KeyError):
-            return self.params.pop(k)
+            return self._params.pop(k)
 
     def drop(self) -> None:
         """If possible, remove this `WikiTemplate` from its parent `WikiText`."""
         if self.parent:
-            self.parent.l.remove(self)
+            self.parent._l.remove(self)
             self.parent = None
 
     def remap(self, old_key: str, new_key: str) -> None:
@@ -256,7 +259,7 @@ class WikiTemplate:
         Returns:
             KeysView: The keys in this WikiTemplate
         """
-        return self.params.keys()
+        return self._params.keys()
 
     def values(self) -> ValuesView:
         """Gets the parameter values in this WikiTemplate
@@ -264,7 +267,7 @@ class WikiTemplate:
         Returns:
             ValuesView: The values contained in this WikiTemplate
         """
-        return self.params.values()
+        return self._params.values()
 
     def as_text(self, indent: bool = False) -> str:
         """Renders this `WikiTemplate` as wikitext, in `str` form.
@@ -276,32 +279,73 @@ class WikiTemplate:
             str: The `WikiTemplate` rendered as wikitext.
         """
         prefix = ("\n" if indent else "") + "|"
-        out = "".join(f"{prefix}{k}={v}" for k, v in self.params.items())
+        out = "".join(f"{prefix}{k}={v}" for k, v in self._params.items())
         if indent:
             out += "\n"
 
         return f"{{{{{self.title}{out}}}}}"
 
-    # TODO: group normalize
+    @staticmethod
+    def normalize(wiki: Wiki, *tl: WikiTemplate) -> None:
+        """Normalizes titles of templates.  This usually fixes capitalization and removes random underscores.
+
+        Args:
+            wiki (Wiki): The Wiki object to use.  The `WikiTemplate` titles will be normalized against this `Wiki`.
+            tl (WikiTemplate): The `WikiTemplate` objects to normalize.
+        """
+        template_ns = wiki.ns_manager.stringify(NS.TEMPLATE)
+        m = {t.title: t for t in tl}
+
+        for k, v in OQuery.normalize_titles(wiki, list(m.keys())).items():
+            m[k].title = wiki.nss(v) if wiki.which_ns(v) == template_ns else v
 
 
 class WParser:
+    """Entry point for the WParser module"""
 
     @staticmethod
-    def parse(wiki: Wiki, title: str = None, text: str = None):
+    def parse(wiki: Wiki, title: str = None, text: str = None) -> WikiText:
+        """Parses the title or text into `WikiText`/`WTemplate` objects.  If `title` and `text` are both specified, then `text` will be parsed as if it was on `title`.
 
+        Args:
+            wiki (Wiki): The Wiki object to use.
+            title (str, optional): The title to use.  If `text` is not specified, then the text of `title` will be automatically fetched and parsed. Defaults to None.
+            text (str, optional): The text to parse. If `title` is specified, then the text will be parsed as if it is was published on `title`. Defaults to None.
+
+        Raises:
+            ValueError: If `title` and `text` are both `None`.
+
+        Returns:
+            WikiText: The result of the parsing operation.  `None` if something went wrong.
+        """
         if not any((title, text)):
             raise ValueError("Either title or text must be specified")
 
-        pl = make_params("parse", {"prop": "parsetree"} | ({"page": title} if title else {"contentmodel": "wikitext", "text": text}))
+        pl = {"prop": "parsetree"}
+        if title:
+            pl["title" if text else "page"] = title
+        if text:
+            pl |= {"contentmodel": "wikitext", "text": text}
+
+        pl = make_params("parse", pl)
+
+        # pl = make_params("parse", {"prop": "parsetree"} | ({"page": title} if title else {"contentmodel": "wikitext", "text": text}))  # TODO: not mutually exlusive
         try:
-            root = ElementTree.fromstring(mine_for(wiki.client.post(wiki.endpoint, data=pl).json(), "parse", "parsetree"))
-            return root
+            return WParser._parse_wiki_text(ElementTree.fromstring(mine_for(wiki.client.post(wiki.endpoint, data=pl).json(), "parse", "parsetree")))
         except Exception:
             log.error("%s: Error occured while querying server with params: %s", wiki, pl, exc_info=True)
 
     @staticmethod
-    def parse_wiki_text(root: ElementTree.Element, flatten: bool = True) -> WikiText:
+    def _parse_wiki_text(root: ElementTree.Element, flatten: bool = True) -> WikiText:
+        """Parses an XML `Element` as `WikiText`
+
+        Args:
+            root (ElementTree.Element): The `Element` to parse
+            flatten (bool, optional): `True` causes flattening of (extract text only) non-`template` tags (e.g. `comment`, `h1`).  `False` causes these to be skipped completely. Defaults to True.
+
+        Returns:
+            WikiText: The resulting `WikiText` from parsing
+        """
         out = WikiText()
 
         if root.text:
@@ -309,9 +353,9 @@ class WParser:
 
         for x in root:
             if x.tag == "template":
-                out += WParser.parse_wiki_template(x)
+                out += WParser._parse_wiki_template(x)
             elif flatten:
-                out += x.text
+                out += WParser._parse_wiki_text(x, flatten)  # handle templates in h1 tags
 
             if x.tail:
                 out += x.tail
@@ -319,25 +363,41 @@ class WParser:
         return out
 
     @staticmethod
-    def parse_wiki_template(root: ElementTree.Element):
+    def _parse_wiki_template(root: ElementTree.Element) -> WikiTemplate:
+        """Parses an XML `Element` as a `WikiTemplate`.
+
+        Args:
+            root (ElementTree.Element): The `Element` to parse
+
+        Returns:
+            WikiTemplate: The resulting `WikiTemplate` from parsing 
+        """
         out = WikiTemplate()
 
         for x in root:
             if x.tag == "title":
-                out.title = str(WParser.parse_wiki_text(x, False))  # handle comments in template title
+                out.title = str(WParser._parse_wiki_text(x, False))  # handles comments in template title <_<
             elif x.tag == "part":
-                out.set_param(*WParser.parse_template_parameter(x))
+                out.set_param(*WParser._parse_template_parameter(x))
 
         return out
 
     @staticmethod
-    def parse_template_parameter(root: ElementTree.Element) -> tuple[str, WikiText]:
+    def _parse_template_parameter(root: ElementTree.Element) -> tuple[str, WikiText]:
+        """Parses an XML `Element` as the parameter of a `WikiTemplate`.
+
+        Args:
+            root (ElementTree.Element): The `Element` to parse 
+
+        Returns:
+            tuple[str, WikiText]:A tuple where the first element is the key of the parameter, and the second element of the tuple is the value of the parameter.
+        """
         key = value = None
 
         for x in root:
             if x.tag == "name":
                 key = x.get("index") or x.text.strip()
             elif x.tag == "value":
-                value = WParser.parse_wiki_text(x)
+                value = WParser._parse_wiki_text(x)
 
         return key, value
---
+++ tests/test_wparser.py
@@ -0,0 +1,8 @@
+
+from .base import QueryTestCase
+
+
+class TestWParser(QueryTestCase):
+
+    def test_sanity(self):
+        pass
