32534a1aec14a0f06cb4e3021dda54a547f77e29
fastily <fastily@users.noreply.github.com>
2021-05-26 18:53:13 -0700

    tag for release 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pwiki",
-    version="1.0.0",
+    version="1.1.0",
     author="Fastily",
     author_email="fastily@users.noreply.github.com",
     description="A Python library for effortlessly interacting with Wikipedia/MediaWiki",
